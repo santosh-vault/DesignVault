bee8267b8f4dd31ce651440b7733f60b397f88d5
Santosh Subedi <santossubedi33@gmail.com>
2024-09-12 23:15:29 +0545

    cv added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,7 +13,17 @@ const HeroSection = () => {
           <a href="mailto:santoshsub.33@gmail.com" className="btn-primary">
             Hire Me
           </a>
-          <button className="btn-secondary">View Work</button>
+          <button
+            className="btn-secondary"
+            onClick={() =>
+              window.open(
+                "https://docs.google.com/document/d/1v6hYYh5opgvjByU7WHhLRbdu3WhgHLd4bnT10cxh5Ss/edit#heading=h.319qcn16p80x",
+                "_blank"
+              )
+            }
+          >
+            Download CV
+          </button>
         </div>
       </div>
     </section>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,9 +4,19 @@ import "./Works.css";
 const Works = () => {
   const works = [
     {
+      img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1726160589/sony_cdujyb.png",
+      text: "A Socail media Post",
+    },
+    {
       img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1726135184/Untitled-1_rqngok.png",
       text: "Thumbnail Design for Client",
     },
+
+    {
+      img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1722184035/ds_ambtew.gif",
+      text: "We can design a motion graphics as well",
+    },
+
     {
       img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1722184028/khn_ltuwru.jpg",
       text: "Worked in Metropolitan city as a freelancer, creating many infographics for their social media and printing purposes.",
@@ -20,6 +30,10 @@ const Works = () => {
       text: "While working at Aaryan Production, I created many social media posts for Luxury Lights.",
     },
     {
+      img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1726160591/18001782_jd560e.png",
+      text: "A logo design for greenify, a tree plantation project",
+    },
+    {
       img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1722184033/bird_jqn30m.jpg",
       text: "This is one of my favorite photo manipulations.",
     },
@@ -31,17 +45,18 @@ const Works = () => {
       img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1722184032/leather_ylcyus.jpg",
       text: "I designed a logo for the popular Nepali online media, Shabda Sanchar.",
     },
+
     {
-      img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1722184033/mero_v0k7t3.jpg",
-      text: "A demo of an ID card designed for Shabda Sanchar.",
+      img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1722184030/poster_rt3cpy.jpg",
+      text: "A mobile cover design.",
     },
     {
       img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1722184028/parsa_uj94wo.jpg",
       text: "While working at Aaryan Production, I collaborated with Beatles to design social media posts.",
     },
     {
-      img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1722184030/poster_rt3cpy.jpg",
-      text: "A mobile cover design.",
+      img: "https://res.cloudinary.com/dvggalwfd/image/upload/v1722184033/mero_v0k7t3.jpg",
+      text: "A demo of an ID card designed for Shabda Sanchar.",
     },
   ];
 
