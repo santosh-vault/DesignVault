badde415fb6c3c8ac10129725e5fec72534fe5f9
ashishbhusal2 <ashishbhusal84@gmail.com>
2025-01-02 11:31:39 +0545

    fixed

--- src/components/UiProjects.jsx
+++ src/components/UiProjects.jsx
@@ -1,31 +1,38 @@
 import React from "react";
 import "./UIProjects.css";
+import {
+  uidesign1,
+  uidesign2,
+  uidesign3,
+  uidesign4,
+  uidesign5,
+} from "../scripts/Images";
 
 const UIProjects = () => {
   const projects = [
     {
       link: "https://www.figma.com/design/Ag57qia7VnNtsZulODF5vD/Fintech?node-id=0-1&p=f&t=We7eYyTqoMAcBXLr-0",
-      img: "src/assets/1.png",
+      img: uidesign1,
       text: "Landing Page Design for a Tech Startup.",
     },
     {
       link: "https://www.figma.com/design/7vkWVTsoooePxShfkhyHJn/award?t=We7eYyTqoMAcBXLr-0",
-      img: "src/assets/2.png",
+      img: uidesign2,
       text: "Dashboard UI for a Finance Application.",
     },
     {
       link: "https://www.figma.com/design/7vkWVTsoooePxShfkhyHJn/award?t=We7eYyTqoMAcBXLr-0",
-      img: "src/assets/3.png",
+      img: uidesign3,
       text: "Mobile App UI for a Food Delivery Service.",
     },
     {
       link: "https://www.figma.com/design/ZB9aNewpdABV68OAr7GvPG/celebrity-website?node-id=0-1&p=f&t=z3qFn2PyDTZzdO0W-0",
-      img: "src/assets/4.png",
+      img: uidesign4,
       text: "E-commerce Website Design with Modern Aesthetics.",
     },
     {
       link: "https://www.figma.com/design/HW1NBJcRD1CszlsxNJogmm/Automobiles?node-id=0-1&p=f&t=z3qFn2PyDTZzdO0W-0",
-      img: "src/assets/5.png",
+      img: uidesign5,
       text: "Portfolio Website for a Creative Agency.",
     },
   ];
---
+++ src/scripts/Images.jsx
@@ -0,0 +1,7 @@
+import uidesign1 from "../assets/1.png";
+import uidesign2 from "../assets/2.png";
+import uidesign3 from "../assets/3.png";
+import uidesign4 from "../assets/4.png";
+import uidesign5 from "../assets/5.png";
+
+export { uidesign1, uidesign2, uidesign3, uidesign4, uidesign5 };
